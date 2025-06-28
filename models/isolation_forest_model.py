044dc3ab68c67361db6d4c0a125549218c656ebf
erenalptunay <tunayerenalp@gmail.com>
2025-06-28 21:53:15 +0300

    Add model status message

--- models/isolation_forest_model.py
+++ models/isolation_forest_model.py
@@ -1,12 +1,14 @@
 from pyod.models.iforest import IForest
 
 
-def run_iforest(X, contamination=0.00172, random_state=42):
-
+def run_iforest(X, contamination=0.002, random_state=42):
+    print("→ [IForest] Fitting model...")
     clf = IForest(contamination=contamination, random_state=random_state)
     clf.fit(X)
 
+    print("→ [IForest] Predicting...")
     y_pred = clf.predict(X)
     scores = clf.decision_scores_
 
+    print("→ [IForest] Done.")
     return y_pred, scores, clf
