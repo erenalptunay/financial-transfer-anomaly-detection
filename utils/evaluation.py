5c1c10697610b7be398a241ce316f70e36041dd0
erenalptunay <tunayerenalp@gmail.com>
2025-06-28 14:02:06 +0300

    Improve evaluate_model_success function

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,14 +1,14 @@
 from sklearn.metrics import accuracy_score
-
+import numpy as np
 
 def evaluate_model_success(y_true, y_pred):
 
     accuracy = accuracy_score(y_true, y_pred)
     total = len(y_true)
-    correct = (y_true == y_pred).sum()
+    correct = np.sum(y_true == y_pred)
 
-    print(f"""
-    ✔️ Correct predictions: {correct} / {total}
-    ❌ Mistaken predictions: {total - correct}
-    ✅ Accuracy: %{accuracy * 100:.2f}
-    """)
+    print(
+        f"✔️ Correct predictions  : {correct} / {total}\n"
+        f"❌ Mistaken predictions : {total - correct}\n"
+        f"✅ Accuracy             : %{accuracy * 100:.2f}"
+    )
